2e20d590adb01076200da54f25e67b3b93fcd276
Cédric Skwar <cdrc@skwar.me>
2022-11-27 17:50:08 +0100

    Fixed it.

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Scrapper
+
+## What is this?
+
+This is a simple web scraper using [geckodriver](https://github.com/mozilla/geckodriver) and [selenium](https://www.selenium.dev/)
--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class Main:
     def __init__(self):
-        self.url_list = ["https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8660502/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646333/"]
+        self.url_list = ["https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8660502/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646333/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648002/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8630787/","https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649744/","https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649753/","https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649761/","https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649762/","https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8657044/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654018/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654092/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654198/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654207/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654211/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8660458/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8660502/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8677618/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8630973/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646320/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646333/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646353/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646363/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648002/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648016/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648019/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8649295/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8649322/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8649368/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8659580/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8659586/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8659600/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8672608/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8672638/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8672685/"]
         self.outfile = f"out-{datetime.datetime.now()}.json"
 
     def write_to_file(self, content):
@@ -55,15 +55,15 @@ class Main:
             # find data
             wait.until(EC.presence_of_element_located((By.CLASS_NAME, "attributes")))
             for element in driver.find_elements(By.CLASS_NAME, "attributes"):
-                data_list.append(element.text.split("\n"))
+                split_element = element.text.split("\n")
+                element_dict = self.list_to_dict(split_element)
+                element_dict["url"] = data
+                data_list.append(element_dict)
                 print(datetime.datetime.now())
 
         driver.close()
-        description = self.list_to_dict(self.flatten(data_list))
-        self.write_to_file(description)
+        self.write_to_file(data_list)
         print(data_list)
-
-
 
 if __name__ == "__main__":
     main_class = Main()
