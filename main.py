3d8e1940b164a6e89cc4dab102fc61f95d0325f1
Cédric Skwar <cdrc@skwar.me>
2022-11-27 17:56:59 +0100

    Main: fixed some pylint errors.

--- main.py
+++ main.py
@@ -4,20 +4,52 @@ Author  Benni347@github.com
 # Misc Imports
 import json
 import datetime
-import time
-import random as rnd
 
 # Selenium imports
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class Main:
     def __init__(self):
-        self.url_list = ["https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8660502/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646333/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648002/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8630787/","https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649744/","https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649753/","https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649761/","https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649762/","https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8657044/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654018/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654092/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654198/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654207/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654211/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8660458/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8660502/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8677618/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8630973/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646320/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646333/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646353/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646363/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648002/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648016/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648019/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8649295/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8649322/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8649368/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8659580/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8659586/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8659600/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8672608/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8672638/","https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8672685/"]
+        self.url_list = [
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8660502/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646333/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648002/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8630787/",
+            "https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649744/",
+            "https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649753/",
+            "https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649761/",
+            "https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8649762/",
+            "https://www.troostwijkauctions.com/de/whiteboard-mit-schiene/03-43350-33214-8657044/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654018/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654092/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654198/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654207/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8654211/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8660458/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8660502/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8677618/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8630973/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646320/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646333/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646353/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8646363/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648002/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648016/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8648019/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8649295/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8649322/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8649368/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8659580/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8659586/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8659600/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8672608/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8672638/",
+            "https://www.troostwijkauctions.com/de/whiteboard/03-43350-33214-8672685/",
+        ]
         self.outfile = f"out-{datetime.datetime.now()}.json"
 
     def write_to_file(self, content):
@@ -28,7 +60,7 @@ class Main:
         """
         json_object = json.dumps(content, indent=4)
 
-        with open(self.outfile, "w") as out:
+        with open(self.outfile, "w", encoding="UTF-8") as out:
             out.write(json_object)
 
     def list_to_dict(self, list_param):
@@ -50,7 +82,7 @@ class Main:
         driver = webdriver.Firefox()
         wait = WebDriverWait(driver, 10)
         data_list = []
-        for i, data in enumerate(self.url_list):
+        for _, data in enumerate(self.url_list):
             driver.get(data)
             # find data
             wait.until(EC.presence_of_element_located((By.CLASS_NAME, "attributes")))
@@ -64,6 +96,7 @@ class Main:
         driver.close()
         self.write_to_file(data_list)
         print(data_list)
+
 
 if __name__ == "__main__":
     main_class = Main()
